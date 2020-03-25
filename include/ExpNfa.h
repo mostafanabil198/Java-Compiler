e80f87f61f48d869e60353957ae4353104d8f820
mostafa <mostafanabil198@gmail.com>
2020-03-25 04:30:12 +0200

    add ExpNfa and handle tokenizing

---
+++ include/ExpNfa.h
@@ -0,0 +1,47 @@
+#ifndef EXPNFA_H
+#define EXPNFA_H
+using namespace std;
+#include <bits/stdc++.h>
+
+#include "Tokenizing.h"
+#include "Graph.h"
+#include "NFA.h"
+#include "RegularDef.h"
+#include "GeneralMethods.h"
+
+#define EPS "eps"
+
+class ExpNfa
+{
+public:
+    ExpNfa();
+    bool construct_automata(string line);
+    bool construct_definition(string id, vector<string> definition);
+    bool construct_nfa();
+    void test_graph(Graph *pGraph);
+    void construct_keywords(vector<string> tokens);
+    void construct_punct(vector<string> tokens);
+    void insert_def(vector<Edge *> edges);
+    Graph *recurse_build(vector<string> tokens, int* i);
+    Graph *recurse_brackets(vector<string> *pVector, int *pInt);
+    Graph *create_graph(string basic_string, int *pInt);
+    Graph *split_token(string basic_string, int *pInt, string h);
+    Graph *create_graph(vector<string> *pVector, string basic_string, int *pInt, string h);
+    Graph *create_graph_from_defintition(Definition* def, int* i, string temp) ;
+    Graph *get_cont(Graph *d_g, vector<string> *tokens, int *i);
+    Graph *expanded_graph(string tokens, int *i);
+    string helper_value(vector<string> *pVector);
+    vector<string> expand_def(string def);
+
+
+protected:
+
+private:
+    vector<Graph*> exp_graphs;
+    GeneralMethods* general_methods;
+    int priority = 1;
+    int node_id = 1;
+    map<string, vector<string>> definitions;
+};
+
+#endif // EXPNFA_H
