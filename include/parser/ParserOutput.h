e8a513cafa4429cec296c362aab195dee7e1210d
nashar <alnasharm778@gmail.com>
2020-04-18 00:24:15 +0200

    working on output

--- include/parser/ParserOutput.h
+++ include/parser/ParserOutput.h
@@ -1,8 +1,9 @@
 #ifndef PARSEROUTPUT_H
 #define PARSEROUTPUT_H
-
+#include "ParserTable.h"
 #include <bits/stdc++.h>
 #include <iostream>
+#include <stack>
 
 using namespace std;
 class ParserOutput
--- src/parser/ParserOutput.cpp
+++ src/parser/ParserOutput.cpp
@@ -4,3 +4,32 @@ ParserOutput::ParserOutput()
 {
 
 }
+
+
+void ParserOutput:: derive(vector<string> tokens)
+{
+
+    parsing_stack.push("$");
+    parsing_stack.push(ParserTable::getInstance()->get_first_non_terminal());
+    int index = 0;
+    while(!parsing_stack.empty())
+    {
+        string top = parsing_stack.top();
+        if(!ParserTable::getInstance()->is_non_terminal(top))
+        {
+            if(top == tokens.at(index))
+            {
+                parsing_stack.pop();
+                cout <<top << " matched with " << tokens.at(index)<<endl;
+                index++;
+            } else {
+                parsing_stack.pop();
+                cout << "Top of stack: " << top << "didnot match with " << tokens.at(index) << endl;
+            }
+        } else {
+
+        }
+    }
+
+}
+
