fe06a3f4897705a78b2fbcb1fa52014fd5b60912
Merna <merna_gohary@yahoo.com>
2020-04-19 23:03:20 +0200

    comments  done

--- src/parser/FollowGenerator.cpp
+++ src/parser/FollowGenerator.cpp
@@ -8,11 +8,13 @@ FollowGenerator::FollowGenerator()
 
 void FollowGenerator:: generate_follow()
 {
-
+    // First, add $ in the follow list of the start non terminal
     ParserTable::getInstance()->add_follow(ParserTable::getInstance()->get_first_non_terminal(),"$");
     get_follow(ParserTable::getInstance()->get_first_non_terminal());
+    // loop for all the nonterminal to get the follow of them
     for(auto production : ParserTable::getInstance()->get_productions_table())
     {
+       // check if the follow of this non terminal still not calculated
         if(!ParserTable::getInstance()->has_follow(production.first))
         {
             get_follow(production.first);
@@ -26,13 +28,16 @@ void FollowGenerator:: get_follow(string non_terminal)
 {
 
     vector<pair<string, pair<int, int>>> current = ParserTable::getInstance()->get_non_terminal_indexing(non_terminal);
+
+    //loop for each position this non terminal existed in and calculate the follow on that position
     for( auto pos : current)
     {
-        //cout << "non_terminal = " << non_terminal << endl;
-        //cout <<"first index = "<< pos.second.first << " & second index = " << pos.second.second <<endl;
         finish = false;
+        //create object of the all productions of that nonterminal to find the dssired position
         vector<vector<string>> cur_productions = ParserTable::getInstance()->get_production(pos.first);
 
+        //if that nonterminal is the last symbol in that production get the follow of the left non termenal
+        //Example : X --> aBb  ,  where x :left nonterminal
         if(pos.second.second == cur_productions.at(pos.second.first).size()-1)
         {
             if(pos.first != non_terminal){
@@ -41,7 +46,6 @@ void FollowGenerator:: get_follow(string non_terminal)
 
             for(auto elem_starts : ParserTable::getInstance()->get_follow(pos.first))
             {
-               // cout << "non terminal = " << non_terminal << "--->" <<elem_starts << endl;
                 ParserTable::getInstance()->add_follow(non_terminal,elem_starts);
             }
             }
@@ -49,54 +53,47 @@ void FollowGenerator:: get_follow(string non_terminal)
         }
         else
         {
-
-            if(pos.first != cur_productions.at(pos.second.first).at(pos.second.second+1))
-            {
-
-                //cout << "alhbd = " << cur_productions.at(pos.second.first).at(pos.second.second+1) << endl;
+         // if that nonterminal is not the last symbol .so, check the next symbol is it terminal or non terminal
                 evaluate_next_production(cur_productions,pos.second.first,pos.second.second+1,non_terminal);
-                //cout << "finish = " << finish << endl;
+                // if the first of the next nonterminal has epsilon , get the follow of the left non terminal
                 if(finish)
                 {
-                  //  cout << "pos.first = " << pos.first <<endl;
-                  //  cout << "terminal = " << cur_productions.at(pos.second.first).at(pos.second.second+1) <<endl;
-
                     if(!ParserTable::getInstance()->has_follow(pos.first))
                     get_follow(pos.first);
 
                     for(auto elem_starts : ParserTable::getInstance()->get_follow(pos.first))
                     {
-                   //     cout << "non terminal = " << non_terminal << "--->" << elem_starts<< endl;
                         ParserTable::getInstance()->add_follow(non_terminal,elem_starts);
                     }
 
                 }
-            }
-
         }
 
 
     }
 }
 
-
+//check the next symbol is terminal or non terminal
+//if terminal put it in the follow list
+//if non terminal get the start of it and check it has epsilon or not
 void FollowGenerator:: evaluate_next_production(vector<vector<string>> current,int i, int j,string non_terminal)
 {
     bool contain_eps = false;
     string production = current.at(i).at(j);
+
+    // if it is terminal , add it in the follow list
     if(!ParserTable::getInstance()->is_non_terminal(production))
     {
-      //  cout << "non terminal = " << non_terminal << "--->" << production<< endl;
         ParserTable::getInstance()->add_follow(non_terminal,production);
     }
     else
     {
+    //if it is nonterminal get the start of it and add them in the follow list
         vector<pair<string, vector<string>>> starts_of_nonterminal = ParserTable::getInstance()->get_start(production);
         for(auto start : starts_of_nonterminal)
         {
             if(!(start.first==EPSILON))
             {
-            //    cout << "non terminal = " << non_terminal << "--->" << start.first<< endl;
                 ParserTable::getInstance()->add_follow(non_terminal,start.first);
 
             }
@@ -105,8 +102,10 @@ void FollowGenerator:: evaluate_next_production(vector<vector<string>> current,i
                 contain_eps = true;
             }
         }
+        //if it has epsilon
         if(contain_eps)
         {
+            //if it is not the last symbol in the production , get in a recursion and check the next symbol
             if(j < current.at(i).size()-1)
             {
                 evaluate_next_production(current,i,j+1,non_terminal);
